744438f9d47a419db02e81a1f5e2e19a01dbd316
OlegAlmagambetov <ol.almagambetov@gmail.com>
2022-11-30 13:33:05 +0100

    source fix

--- AbstrakteKlassen/Zoo/ZooMain.java
+++ AbstrakteKlassen/Zoo/ZooMain.java
@@ -1,36 +1,35 @@
 package Zoo;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class ZooMain {
+public class ZooMain{
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		Tier t1 = new Hund("Boby",1);
-		//Tier t2 = new Tier("Dennis",40); //Abstrakte Klassen können nicht instanziert werden
-		
-		//Factory Method Pattern
-		Tier tier = null;
-		String spezies ="...";
-		int alter = 0;
-		String name ="...";
-		
-		System.out.println("Tier: ");
-		spezies = sc.nextLine();
-		System.out.println("Name: ");
-		name = sc.nextLine();
-		System.out.println("Alter: ");
-		alter = sc.nextInt();
-		
-		switch(spezies) {
-		case "Hund":
-			tier = new Hund(name,alter);break;
-		default:
-			System.out.println("Spezies unbekannt");break;
-		}
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Tier t1 = new Hund("Boby",1);
+        //Tier t2 = new Tier("Dennis",40); //Abstrakte Klassen können nicht instanziert werden
+
+        //Factory Method Pattern
+        Tier tier = null;
+        String spezies ="...";
+        int alter = 0;
+        String name ="...";
+
+        System.out.println("Tier: ");
+        spezies = sc.nextLine();
+        System.out.println("Name: ");
+        name = sc.nextLine();
+        System.out.println("Alter: ");
+        alter = sc.nextInt();
+
+        switch(spezies) {
+            case "Hund":
+                tier = new Hund(name,alter);break;
+            default:
+                System.out.println("Spezies unbekannt");break;
+        }
+
+    }
 } 
